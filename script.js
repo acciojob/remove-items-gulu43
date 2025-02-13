04337cbca9badf0c96863d9f61b0f80dd5edafdf
gulu43 <145833927+gulu43@users.noreply.github.com>
2025-02-13 19:01:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let btn = document.getElementById('btn');
+let colorSelect = document.getElementById('colorSelect');
+
+btn.addEventListener('click', function () {
+    let selectedOption = colorSelect.options[colorSelect.selectedIndex];
+    if (selectedOption) {
+        console.log('Removing:', selectedOption.value);
+        selectedOption.remove(); // Remove the selected option
+    }
+});
