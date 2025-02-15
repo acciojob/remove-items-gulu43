89ecac2e3d4bfbec3cf7c380e476da49c1d32fe7
gulu43 <145833927+gulu43@users.noreply.github.com>
2025-02-15 08:21:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-//your JS code here. If required.
 let btn = document.getElementById('btn');
 let colorSelect = document.getElementById('colorSelect');
 
-let g_index = 0;
-colorSelect.addEventListener('change', function() {
-	let index = colorSelect.selectedIndex;
-	g_index = index;
-})
-
 btn.addEventListener('click', function () {
-	// or g_index.remove();
-    let selectedOption = colorSelect.options[g_index];
-    selectedOption.remove()
+    let selectedIndex = colorSelect.selectedIndex;
+    if (selectedIndex !== -1) {  // Ensure something is selected
+        colorSelect.remove(selectedIndex);
+    }
 });
